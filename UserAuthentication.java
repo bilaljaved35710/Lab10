1795fa2c45b65c3c004e30cd3e7b5593e79f27d3
Bilal_Javed <F24608016@nutech.edu.pk>
2026-05-17 19:06:32 +0500

    Create UserAuthentication.java

---
+++ UserAuthentication.java
@@ -0,0 +1,22 @@
+public class UserAuthentication {
+
+    public boolean login(String username, String password) {
+
+        if (username.equals("admin") && password.equals("1234")) {
+            System.out.println("Login successful");
+            return true;
+        } else {
+            System.out.println("Invalid username or password");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        UserAuthentication auth = new UserAuthentication();
+
+        boolean result = auth.login("admin", "1234");
+
+        System.out.println("Result: " + result);
+    }
+}
