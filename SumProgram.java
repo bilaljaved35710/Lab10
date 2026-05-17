e41a4e7ec8542df0c9d744ecc737e9a18f0a8f54
Bilal_Javed <F24608016@nutech.edu.pk>
2026-05-17 18:38:11 +0500

    Create SumProgram.java

---
+++ SumProgram.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumProgram {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
+        input.close();
+    }
+}
+
